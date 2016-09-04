d00dfa030709445e998c3877fc722446e130d8a3
Hoa Ho Van <hoahv.vn@gmail.com>
2016-09-04 13:13:35 +0700

    building short code

--- wp-term.php
+++ wp-term.php
@@ -15,42 +15,11 @@
   
 */
 
-/* !0. TABLE OF CONTENTS */
-
-/*
- 		1. HOOKS
- 			1.1 - Registers all our custom shortcodes
- 		2. SHORTCODES
- 			2.1 - slb_register_shortcodes()
- 			2.2 - slb_form_shortcode()
- 		3. FILTERS
- 		4. EXTERNAL SCRIPTS
- 		5. ACTIONS
- 		6. HELPERS
- 		7. CUSTOM POST TYPES
- 		8. ADMIN PAGES
- 		9. SETTINGS
-
- */
-
 /* !1. HOOKS */
-
-// 1.1
-// Hint: Registers all our custom shortcodes on init
-add_action( 'init', 'slb_register_shortcodes');
-
 
 /* !2. SHORTCODES */
 
-// 2.1 
-function slb_register_shortcodes() {
-
-	add_shortcode( 'slb_form', 'slb_form_shortcode' );
-
-}
-
-// 2.2
-function slb_form_shortcode( $args, $content="" ) {
+function slb_form( $args, $content="" ) {
 
 	// Setup our output variable - the format html
 	$output = '
@@ -66,15 +35,9 @@ function slb_form_shortcode( $args, $content="" ) {
 				<p class="slb-input-container">
 					<label>Your Email</label> <br>
 					<input type="email" name="slb_email" value="" placeholder="ex. you@email.com" />
-				</p>';
+				</p>
 
-				// Including content in our form html if content is passed into the function
-				if( strlen($content) ) {
-					$output .= '<div class="slb-content">' . wpautop($content) . '</div>';
-				}
-
-				// Completing our form html
-				$output .= '<p class="slb-input-container">
+				<p class="slb-input-container">
 					<input type="submit" name="slb_submit" value="Sign Me Up" />
 				</p>
 
