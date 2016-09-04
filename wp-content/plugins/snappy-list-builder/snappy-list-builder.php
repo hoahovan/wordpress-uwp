7453191bacd179d3dd30623d27f7a8df0caa7b9e
Hoa Ho Van <hoahv.vn@gmail.com>
2016-09-04 10:54:56 +0700

    initial unlimate wordpress plugin

---
+++ wp-content/plugins/snappy-list-builder/snappy-list-builder.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Plugin Name: Snappy List Builder
+ * Plugin URI: http://wordpressplugincourse/plugins/snappy-list-builder
+ * Description: The ultimate email list building plugin for Wordpress. 
+ 	 Capture new subscribers. Reward subscriber with a custom download upon opt-in. Build unlimited lists. 
+ 	 Import and export subscribers easily with .csv
+ * Version: 1.0
+ * Author: Joel Funk @ Code College
+ * Author URI: http://joelfunk.codecollege.ca
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text domain: snappy-list-builder
+  
+*/
